4fbc74c5932aef572d54291a77969b89516a0139
MousseDm <e1297764@u.nus.edu>
2025-10-09 16:44:22 +0800

    Add javadocs for ListCommand and ListCommandParser

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -5,6 +5,7 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
 
+/** Lists all persons and reports the number listed. */
 public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
     public static final String MESSAGE_SUCCESS = COMMAND_WORD + ": Lists all persons.\nUsage: " + COMMAND_WORD;
--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -5,7 +5,9 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
+/** Parses input arguments and creates a ListCommand. */
 public class ListCommandParser implements Parser<ListCommand> {
+    /** Parses args and returns a ListCommand, rejecting any extraneous tokens. */
     public ListCommand parse(String args) throws ParseException {
         if (!args.trim().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_SUCCESS));
