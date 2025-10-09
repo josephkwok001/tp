f958820cf2fffb1161912a6608223603a592d088
Joseph <177612090+josephkwok001@users.noreply.github.com>
2025-10-09 22:29:55 +0800

    feat: Add find tag feature

--- src/main/java/seedu/address/model/person/TagContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/TagContainsKeywordsPredicate.java
@@ -6,11 +6,13 @@ import java.util.function.Predicate;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
-
-public class TagContainsKeywordsPredicate implements Predicate<Person>{
+/**
+ * Tests that a {@code Person}'s {@code Tag} matches any of the keywords given.
+ */
+public class TagContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
 
-    public TagContainsKeywordsPredicate (List<String> keywords) {
+    public TagContainsKeywordsPredicate(List<String> keywords) {
         this.keywords = keywords;
     }
 
