37a3393c3e1fe0c5d3b82c3fdedeba875477e5db
weixian <e1398114@u.nus.edu>
2025-10-09 21:42:10 +0800

    Add Listing field to Person

--- src/main/java/seedu/address/model/person/Listing.java
+++ src/main/java/seedu/address/model/person/Listing.java
@@ -1,18 +1,37 @@
 package seedu.address.model.person;
 
+/**
+ * Represents a Person's listing in the address book.
+ * Guarantees: immutable; is always valid
+ */
 public class Listing {
 
-    private final String listing;
+    public final String value;
 
+    /**
+     * Constructs a {@code Listing}.
+     *
+     * @param listing A valid listing.
+     */
     public Listing(String listing) {
-        this.listing = listing;
+        this.value = listing;
     }
 
+    /**
+     * Returns the listing value.
+     */
     public String getListing() {
-        return listing;
+        return value;
+    }
+
+    /**
+     * Returns true if a given string is a valid listing.
+     */
+    public static boolean isValidListing(String test) {
+        return true;
     }
 
     public String toString() {
-        return listing;
-        }
+        return value;
+    }
 }
