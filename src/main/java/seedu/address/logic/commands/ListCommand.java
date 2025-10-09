bd3a642bb742149757c760da53a6ff0ef0ce88d3
MousseDm <143419381+MousseDm@users.noreply.github.com>
2025-10-09 12:16:00 +0100

    Revert "Feature/list enhancement"

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -5,16 +5,20 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
 
-/** Lists all persons and reports the number listed. */
+/**
+ * Lists all persons in the address book to the user.
+ */
 public class ListCommand extends Command {
+
     public static final String COMMAND_WORD = "list";
-    public static final String MESSAGE_SUCCESS = COMMAND_WORD + ": Lists all persons.\nUsage: " + COMMAND_WORD;
+
+    public static final String MESSAGE_SUCCESS = "Listed all persons";
+
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        int count = model.getFilteredPersonList().size();
-        return new CommandResult("Listed " + count + " persons");
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
