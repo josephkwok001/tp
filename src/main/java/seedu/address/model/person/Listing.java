c4ea94a5a58e38d1b5ab23365ee83c05b30315f3
weixian <e1398114@u.nus.edu>
2025-10-09 20:47:21 +0800

    Add Listing class

---
+++ src/main/java/seedu/address/model/person/Listing.java
@@ -0,0 +1,18 @@
+package seedu.address.model.person;
+
+public class Listing {
+
+    private final String listing;
+
+    public Listing(String listing) {
+        this.listing = listing;
+    }
+
+    public String getListing() {
+        return listing;
+    }
+
+    public String toString() {
+        return listing;
+        }
+}
