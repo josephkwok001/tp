8e08f1eb6a154d220cfef78076e9d7634efd75f9
ebt-wu <edbertwu08@gmail.com>
2025-10-16 22:42:11 +0800

    Add Property class

---
+++ src/main/java/seedu/address/model/property/Property.java
@@ -0,0 +1,126 @@
+package seedu.address.model.property;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+import seedu.address.model.person.Person;
+
+/**
+ * Represents a Property in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Property {
+    private final Person owner;
+    private final Address address;
+    private final Price price;
+    private final List<Person> interestedPersons;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Property(Person owner, Address address, Price price) {
+        this(owner, address, price, new ArrayList<>());
+    }
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Property(Person owner, Address address, Price price, List<Person> interestedPersons) {
+        requireAllNonNull(owner, address, price, interestedPersons);
+        this.owner = owner;
+        this.address = address;
+        this.price = price;
+        this.interestedPersons = new ArrayList<>(interestedPersons);
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
+
+    /**
+     * Returns an unmodifiable view of the interested persons list.
+     */
+    public List<Person> getInterestedPersons() {
+        return Collections.unmodifiableList(interestedPersons);
+    }
+
+    /**
+     * Returns true if the person is interested in this property.
+     */
+    public boolean hasInterestedPerson(Person person) {
+        requireAllNonNull(person);
+        return interestedPersons.stream().anyMatch(p -> p.equals(person));
+    }
+
+    /**
+     * Adds an interested person to this property.
+     * The person must not be the owner of the property.
+     */
+    public Property addInterestedPerson(Person person) {
+        requireAllNonNull(person);
+        if (person.equals(owner)) {
+            throw new IllegalArgumentException("Owner cannot be added as an interested person");
+        }
+        List<Person> updatedList = new ArrayList<>(interestedPersons);
+        updatedList.add(person);
+        return new Property(owner, address, price, updatedList);
+    }
+
+    /**
+     * Removes an interested person from this property.
+     */
+    public Property removeInterestedPerson(Person person) {
+        requireAllNonNull(person);
+        List<Person> updatedList = new ArrayList<>(interestedPersons);
+        updatedList.remove(person);
+        return new Property(owner, address, price, updatedList);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Property)) {
+            return false;
+        }
+
+        Property otherProperty = (Property) other;
+        return owner.equals(otherProperty.owner)
+                && address.equals(otherProperty.address)
+                && price.equals(otherProperty.price)
+                && interestedPersons.equals(otherProperty.interestedPersons);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(owner, address, price, interestedPersons);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Property owned by: ")
+                .append(getOwner().getName())
+                .append(" Address: ")
+                .append(getAddress())
+                .append(" Price: $")
+                .append(getPrice())
+                .append(" Interested Persons: ")
+                .append(getInterestedPersons().size());
+        return builder.toString();
+    }
+}
