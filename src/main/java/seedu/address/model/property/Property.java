c8b295c2b1f9fa922b26730f31c6aa0fdf206897
ebt-wu <edbertwu08@gmail.com>
2025-10-17 00:19:45 +0800

    Add Property class and its functionalities

--- src/main/java/seedu/address/model/property/Property.java
+++ src/main/java/seedu/address/model/property/Property.java
@@ -2,42 +2,41 @@ package seedu.address.model.property;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
 import java.util.Objects;
 
-import seedu.address.model.person.Person;
+// import seedu.address.model.person.Person;
 
 /**
  * Represents a Property in the address book.
- * Guarantees: details are present and not null, field values are validated, immutable.
+ * Guarantees: details are present and not null, field values are validated,
+ * immutable.
  */
 public class Property {
-    private final Person owner;
+
+    // private final Person owner;
+    private final String owner;
     private final Address address;
     private final Price price;
-    private final List<Person> interestedPersons;
+    // private final List<Person> interestedPersons;
+    private final String propertyName;
 
     /**
      * Every field must be present and not null.
      */
-    public Property(Person owner, Address address, Price price) {
-        this(owner, address, price, new ArrayList<>());
-    }
-
-    /**
-     * Every field must be present and not null.
-     */
-    public Property(Person owner, Address address, Price price, List<Person> interestedPersons) {
-        requireAllNonNull(owner, address, price, interestedPersons);
+    public Property(String owner, Address address, Price price, String propertyName) {
+        // , List<Person> interestedPersons) {
+        requireAllNonNull(owner, address, price); // , interestedPersons);
         this.owner = owner;
         this.address = address;
         this.price = price;
-        this.interestedPersons = new ArrayList<>(interestedPersons);
+        this.propertyName = propertyName;
+        // this.interestedPersons = new ArrayList<>(interestedPersons);
     }
 
-    public Person getOwner() {
+    public String getOwner() {
         return owner;
     }
 
@@ -49,43 +48,61 @@ public class Property {
         return price;
     }
 
-    /**
-     * Returns an unmodifiable view of the interested persons list.
-     */
-    public List<Person> getInterestedPersons() {
-        return Collections.unmodifiableList(interestedPersons);
+    public String getPropertyName() {
+        return propertyName;
     }
 
-    /**
-     * Returns true if the person is interested in this property.
-     */
-    public boolean hasInterestedPerson(Person person) {
-        requireAllNonNull(person);
-        return interestedPersons.stream().anyMatch(p -> p.equals(person));
-    }
+    // /**
+    // * Returns an unmodifiable view of the interested persons list.
+    // */
+    // public List<Person> getInterestedPersons() {
+    // return Collections.unmodifiableList(interestedPersons);
+    // }
+
+    // /**
+    // * Returns true if the person is interested in this property.
+    // */
+    // public boolean hasInterestedPerson(Person person) {
+    // requireAllNonNull(person);
+    // return interestedPersons.stream().anyMatch(p -> p.equals(person));
+    // }
+
+    // /**
+    // * Adds an interested person to this property.
+    // * The person must not be the owner of the property.
+    // */
+    // public Property addInterestedPerson(Person person) {
+    // requireAllNonNull(person);
+    // if (person.equals(owner)) {
+    // throw new IllegalArgumentException("Owner cannot be added as an interested
+    // person");
+    // }
+    // List<Person> updatedList = new ArrayList<>(interestedPersons);
+    // updatedList.add(person);
+    // return new Property(owner, address, price, updatedList);
+    // }
+
+    // /**
+    // * Removes an interested person from this property.
+    // */
+    // public Property removeInterestedPerson(Person person) {
+    // requireAllNonNull(person);
+    // List<Person> updatedList = new ArrayList<>(interestedPersons);
+    // updatedList.remove(person);
+    // return new Property(owner, address, price, updatedList);
+    // }
 
     /**
-     * Adds an interested person to this property.
-     * The person must not be the owner of the property.
+     * Returns true if both persons have the same name.
+     * This defines a weaker notion of equality between two persons.
      */
-    public Property addInterestedPerson(Person person) {
-        requireAllNonNull(person);
-        if (person.equals(owner)) {
-            throw new IllegalArgumentException("Owner cannot be added as an interested person");
+    public boolean isSameProperty(Property otherProperty) {
+        if (otherProperty == this) {
+            return true;
         }
-        List<Person> updatedList = new ArrayList<>(interestedPersons);
-        updatedList.add(person);
-        return new Property(owner, address, price, updatedList);
-    }
 
-    /**
-     * Removes an interested person from this property.
-     */
-    public Property removeInterestedPerson(Person person) {
-        requireAllNonNull(person);
-        List<Person> updatedList = new ArrayList<>(interestedPersons);
-        updatedList.remove(person);
-        return new Property(owner, address, price, updatedList);
+        return otherProperty != null
+                && otherProperty.getPropertyName().equals(getPropertyName());
     }
 
     @Override
@@ -102,25 +119,28 @@ public class Property {
         return owner.equals(otherProperty.owner)
                 && address.equals(otherProperty.address)
                 && price.equals(otherProperty.price)
-                && interestedPersons.equals(otherProperty.interestedPersons);
+                && propertyName.equals(otherProperty.propertyName);
+        // && interestedPersons.equals(otherProperty.interestedPersons);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(owner, address, price, interestedPersons);
+        return Objects.hash(owner, address, price); // , interestedPersons);
     }
 
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
         builder.append("Property owned by: ")
-                .append(getOwner().getName())
+                .append(getOwner())
                 .append(" Address: ")
                 .append(getAddress())
                 .append(" Price: $")
                 .append(getPrice())
-                .append(" Interested Persons: ")
-                .append(getInterestedPersons().size());
+                .append(" Property Name: ")
+                .append(getPropertyName());
+        // .append(" Interested Persons: ")
+        // .append(getInterestedPersons().size());
         return builder.toString();
     }
 }
