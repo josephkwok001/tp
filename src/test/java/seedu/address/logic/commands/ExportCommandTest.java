ede256767038fb433ffc6782c232f1304e2c4aba
Joseph <177612090+josephkwok001@users.noreply.github.com>
2025-10-30 19:34:40 +0800

    Add testcase for code coverage

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -2,7 +2,9 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalProperties.PROPERTY_A;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -92,6 +94,42 @@ public class ExportCommandTest {
         assertEquals(true, file.exists());
 
         file.delete();
+    }
+
+    @Test
+    public void execute_exportWithProperties_includesPropertiesInFile() throws Exception {
+        Model model = new ModelManager(new AddressBook(), new UserPrefs());
+
+        model.addPerson(ALICE);
+        model.addProperty(PROPERTY_A);
+
+        String filename = "test_property_export";
+        ExportCommand command = new ExportCommand(filename);
+
+        command.execute(model);
+
+        File file = new File(System.getProperty("user.dir") + "/data/" + filename + ".csv");
+        assertTrue(file.exists());
+
+        file.delete();
+    }
+
+    @Test
+    public void execute_readOnlyDirectory_throwsCommandException() throws Exception {
+        Model model = new ModelManager(new AddressBook(), new UserPrefs());
+        model.addPerson(ALICE);
+
+        File readOnlyDir = new File(System.getProperty("user.dir") + "/data/readonly");
+        readOnlyDir.mkdirs();
+        readOnlyDir.setReadOnly();
+
+        String filename = "readonly/test_export";
+        ExportCommand command = new ExportCommand(filename);
+
+        assertThrows(CommandException.class, () -> command.execute(model));
+
+        readOnlyDir.setWritable(true);
+        readOnlyDir.delete();
     }
 
 }
