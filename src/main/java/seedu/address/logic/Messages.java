a5f69f44167af98e80ac725572bd2f9cfb15278a
ebt-wu <edbertwu08@gmail.com>
2025-11-03 13:28:32 +0800

    Fix error message to the correct one

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -18,11 +18,11 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_PROPERTY_LISTED_OVERVIEW = "%1$d properties listed!";
-    public static final String MESSAGE_INVALID_PROPERTY_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_INVALID_PROPERTY_DISPLAYED_INDEX = "The property index provided is invalid";
     public static final String MESSAGE_INVALID_OUT_OF_RANGE_INDEX = "The index provided is out of range";
     public static final String MESSAGE_PROPERTY_NOT_FOUND = "The property %1$s could not be found.";
-    public static final String MESSAGE_DUPLICATE_FIELDS =
-                "Multiple values specified for the following single-valued field(s): ";
+    public static final String MESSAGE_DUPLICATE_FIELDS = "Multiple values specified for the"
+        + " following single-valued field(s): ";
 
     /**
      * Returns an error message indicating the duplicate prefixes.
@@ -30,8 +30,7 @@ public class Messages {
     public static String getErrorMessageForDuplicatePrefixes(Prefix... duplicatePrefixes) {
         assert duplicatePrefixes.length > 0;
 
-        Set<String> duplicateFields =
-                Stream.of(duplicatePrefixes).map(Prefix::toString).collect(Collectors.toSet());
+        Set<String> duplicateFields = Stream.of(duplicatePrefixes).map(Prefix::toString).collect(Collectors.toSet());
 
         return MESSAGE_DUPLICATE_FIELDS + String.join(" ", duplicateFields);
     }
